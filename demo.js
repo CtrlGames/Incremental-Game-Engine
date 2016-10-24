90b8ae997458111bed44130a3d5a92747d5670b0
Taylor J Miller <kenostik@gmail.com>
2016-10-24 00:38:14 -0600

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,19 +1,128 @@
 console.log('Demo Connected');
 
+var g = new IncrementalEngine();
+
 var begin = document.getElementById('beginGame');
-var stop = document.getElementById('stopGame');
 var tickCounter = document.getElementById('tickCounter');
+var addWood = document.getElementById('getWood');
+var addFood = document.getElementById('getFood');
+var addWorker = document.getElementById('getWorker');
+var addLumberjack = document.getElementById('lumberjack');
+var addFarmer = document.getElementById('farmer');
+var listSection = document.getElementById('listSection');
 
 console.log(begin);
 
-var beginMyGame = function() {
-	var g = new IncrementalEngine();
+addWood.style.display = "none";
+addFood.style.display = "none";
+addWorker.style.display ="none";
+addLumberjack.style.display ="none";
+addFarmer.style.display ="none";
+
+var beginMyGame = function() {	
 	console.log('Game begun', g);
-	g.begin(1000);
+	g.begin(500);
+	g.createResource('wood');
+	g.createResource('food');
+	addWood.style.display = "inline-block";
+	addFood.style.display = "inline-block";
+	begin.style.display = "none";
 }
 
 begin.onclick = function() {
 	beginMyGame();
 }
 
-stop.onclick = function(){stopTick('game')}
+addWood.onclick = function(){
+	g.increaseResource('wood', 1)
+	if (g.resources.wood.amount >= 5 && g.resources.food.amount >= 5) {
+		g.createResource('worker');
+		addWorker.style.display = "inline-block";
+	}
+};
+addFood.onclick = function() {
+	g.increaseResource('food', 1);
+	if (g.resources.wood.amount >= 5 && g.resources.food.amount >= 5) {
+		g.createResource('worker');
+		addWorker.style.display = "inline-block";
+	}
+}
+
+addWorker.onclick = function(){
+	g.increaseResource('worker', 1);
+	g.addResourceCondition(false, 'food', 2);
+	g.decreaseResource('food', 5);
+	if (g.resources.worker.amount > 0 ) {
+		g.createResource('Lumber Jack');
+		addLumberjack.style.display="inline-block";
+		g.createResource('Farmer');
+		addFarmer.style.display="inline-block";
+	}
+	enableButton(addLumberjack);
+	enableButton(addFarmer);
+};
+addLumberjack.onclick = function(){
+	g.addResourceCondition(true, 'wood', 2);
+	g.decreaseResource('worker', 1);
+	g.increaseResource('Lumber Jack', 1);
+	if (g.resources.worker.amount <= 0) {
+		disableButton(addLumberjack);
+		disableButton(addFarmer);
+	}
+};
+addFarmer.onclick = function(){
+	g.addResourceCondition(true, 'food', 3);
+	g.decreaseResource('worker', 1);
+	g.increaseResource('Farmer', 1);
+	if (g.resources.worker.amount <= 0) {
+		disableButton(addLumberjack);
+		disableButton(addFarmer);
+	}
+};
+
+var populateResourceList = function() {
+	var oldList = document.getElementById('resourceList');
+	listSection.appendChild(oldList);
+    var list = document.createElement('ul');
+
+	for (var key in g.resources) {
+		var r = g.resources[key].name;
+		var a = g.resources[key].amount;
+		var item = document.createElement('li');
+		item.appendChild(document.createTextNode(r + " : " + a));
+		list.appendChild(item);
+	}
+	
+	listSection.replaceChild(list, oldList);
+	listSection.appendChild(list);
+	list.setAttribute('id', 'resourceList');
+}
+
+var checkConditions = function(){
+	for (var key in g.resources) { // This.resources is undefined?
+			if (g.resources[key].conditions.add > 0) {
+				g.increaseResource(key, g.resources[key].conditions.add);
+			}
+			if (g.resources[key].conditions.minus > 0) {
+				g.decreaseResource(key, g.resources[key].conditions.minus);
+			}
+		}
+}
+
+var gameTick = function(){
+    tickCounter.innerHTML = gameTick;
+    populateResourceList();
+    checkConditions();
+}
+
+document.addEventListener('tick', gameTick);
+
+// Helper Funcs~
+
+disableButton = function(elm){
+	elm.disabled = true;
+}
+
+enableButton = function(elm) {
+	elm.disabled = false;
+}
--- engine.js
+++ engine.js
@@ -18,12 +18,14 @@ console.log('Engine connected');
 //Create Game Instance
 window.IncrementalEngine = function IncrementalEngine() {
 
-	this.gameInterval = null;
+	this.gameInterval = 0;
 	this.gameTick = 0;
 	this.resources = {};
 	this.tickConditions =[];
 
 };
+
+console.log(window.IncrementalEngine);
 
 IncrementalEngine.prototype = {
 	//Begin a game session with Tick Interval in milliseconds
@@ -35,7 +37,10 @@ IncrementalEngine.prototype = {
 	onTick() {
 		Number.isInteger(this.gameTick) ? this.gameTick += 1 : this.gameTick = 0;
 
-		for (var key in this.resources) {
+		var event = new Event('tick', {tick: this.gameTick});
+		document.dispatchEvent(event);
+
+		for (var key in this.resources) { // This.resources is undefined?
 			if (this.resources.key.conditions.add > 0) {
 				this.increaseResource(key, this.resources.key.conditions.add);
 			}
@@ -45,32 +50,34 @@ IncrementalEngine.prototype = {
 		}
 	},
 
-	addTickCondition(method, r, n){
-		condition = {
-			method: method,
-			r: r,
-			n: n
-		};
-		this.tickConditions.push(condition);
+	// Add a condition to a resource
+	addResourceCondition(add, r, n){
+		if (add) {
+			this.resources[r].conditions.add += n;
+		} else {
+			this.resources[r].conditions.minus += n;
+		}
 
-		return this.tickConditions;
+		return this.resources.r;
 	},
-
+	// Create a resource. Start with none, and no Conditions
 	createResource(r){
-		this.resources[r] = {
-			name: r,
-			amount: 0,
-			conditions: {add: 0, minus: 0}
+		if (!this.resources[r]) {
+			this.resources[r] = {
+				name: r,
+				amount: 0,
+				conditions: {add: 0, minus: 0}
 
-		};
+			};
+		}
 	},
 
 	increaseResource(r, n) {
-		this.resources.r.amount += n ? n : 1;
+		this.resources[r].amount += n ? n : 1;
 	},
 
-	decreaseResouce(r, n) {
-		this.resourcees.r.amount -= n ? n : 1;
+	decreaseResource(r, n) {
+		this.resources[r].amount -= n ? n : 1;
 	},
 };
 

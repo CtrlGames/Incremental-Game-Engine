9ae236d1e8821b5ade05843279c199cb35188334
Taylor J Miller <kenostik@gmail.com>
2016-11-01 01:48:55 -0600

    Engine updates
    Made changes to Pool Prototype. Added Min/Max amount, modifyRequirements, modifyDetails,  && modifyPoolIncrements. Extended ModifyPoolAmount into different methods that checkRequirements and checkMinandMax

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
 var g = new IncrementalEngine();
 
+// UI Variables
 var begin = document.getElementById('beginGame');
 var tickCounter = document.getElementById('tickCounter');
 var addWood = document.getElementById('getWood');
@@ -8,85 +9,123 @@ var addWorker = document.getElementById('getWorker');
 var addLumberjack = document.getElementById('lumberjack');
 var addFarmer = document.getElementById('farmer');
 var listSection = document.getElementById('listSection');
+var addCustomDiv = document.getElementById('addCustom');
+var custResource = document.getElementById('custResource');
+var custAmount = document.getElementById('custAmount');
+var submitCustom = document.getElementById('submitCustom');
 
-console.log(begin);
+// session variables
+var tickTimer = 0;
+var offset = true;
 
+// Pool List
+var myPools = {
+	wood: {
+		key: 'wood',
+		name: 'Wood',
+		details: ['The Building block of Everything', 'How much could a Woodchuck chuck?']
+	},
+	food: {
+		key: 'food',
+		name: 'Food',
+		minimum: 10,
+		details: {notes: ['Who is Hungry?']}
+	},
+	worker: {
+		key: 'worker',
+		name: 'Worker',
+		requirements: {food: 10},
+		increments: {food: -1}
+	},
+	farmer: {
+		key: 'farmer',
+		name: 'Farmer',
+		increments: {food: .5},
+		requirements: {worker: 1},
+		details: {notes: ['Without us, the world would die!']}
+	},
+	lumberjack: {
+		key: 'lumberjack',
+		name: 'Lumber-jack',
+		requirements: {food: 10, wood: 5, worker: 1},
+		increments: {wood: 5, food: -3}
+	}
+
+}
+
+
+// Hide/Show Settings
 addWood.style.display = "none";
 addFood.style.display = "none";
 addWorker.style.display ="none";
 addLumberjack.style.display ="none";
 addFarmer.style.display ="none";
+addCustomDiv.style.display = "none";
 
-var beginMyGame = function() {	
+begin.onclick = function() {	
 	console.log('Game begun', g);
 	g.begin(500);
-	g.createResource('wood');
-	g.createResource('food');
+	g.addPool(myPools.food);
+	g.addPool(myPools.wood);
 	addWood.style.display = "inline-block";
 	addFood.style.display = "inline-block";
+	addCustomDiv.style.display = "inline-block"
 	begin.style.display = "none";
 }
 
-begin.onclick = function() {
-	beginMyGame();
+submitCustom.onclick = function() {
+	var n = parseInt(custAmount.value);
+	var val = custResource.value;
+	if (val == "interval") {
+		g.modifyQueueInterval(n);
+	} else {
+			addToPool(val, n);
+	}
 }
-
 
 addToPool = function(p, n) {
-	g.pools[p].addToPool((n ? n : 1));
+	var s = g.pools[p].modifyPoolAmount((n ? n : 1));
+	if (!s.success) {
+		console.error("Couldn't Add to " + p + " because " + (s.surplus ? "you are beyond Capacity":"of unmet requirements"), s);
+	}
 }
 
-
-
-
-
-
-
-
-
-
 addWood.onclick = function(){
-	addToPool('wood', 1);
-	if (g.resources.wood.amount >= 5 && g.resources.food.amount >= 5) {
-		g.addPool({name: "worker"});
+	addToPool(myPools.wood.key, 1);
+};
+
+addFood.onclick = function() {
+	addToPool(myPools.food.key, 1);
+	if (g.pools.food.amount >= 5) {
+		g.addPool(myPools.worker);
 		addWorker.style.display = "inline-block";
 	}
 };
-addFood.onclick = function() {
-	g.increaseResource('food', 1);
-	if (g.resources.wood.amount >= 5 && g.resources.food.amount >= 5) {
-		g.createResource('worker');
-		addWorker.style.display = "inline-block";
-	}
-}
 
 addWorker.onclick = function(){
-	g.increaseResource('worker', 1);
-	g.addResourceCondition(false, 'food', 2);
-	g.decreaseResource('food', 5);
-	if (g.resources.worker.amount > 0 ) {
-		g.createResource('Lumber Jack');
+	addToPool(myPools.worker.key, 1);
+	if (g.pools.worker.amount > 0) {
+		g.addPool(myPools.lumberjack);
 		addLumberjack.style.display="inline-block";
-		g.createResource('Farmer');
+	}
+	if (g.pools.worker.amount > 0) {
+		g.addPool(myPools.farmer);
 		addFarmer.style.display="inline-block";
 	}
 	enableButton(addLumberjack);
 	enableButton(addFarmer);
 };
 addLumberjack.onclick = function(){
-	g.addResourceCondition(true, 'wood', 2);
-	g.decreaseResource('worker', 1);
-	g.increaseResource('Lumber Jack', 1);
-	if (g.resources.worker.amount <= 0) {
+	addToPool(myPools.worker.key, -1);
+	addToPool(myPools.lumberjack.key, 1);
+	if (g.pools.worker.amount <= 0) {
 		disableButton(addLumberjack);
 		disableButton(addFarmer);
 	}
 };
 addFarmer.onclick = function(){
-	g.addResourceCondition(true, 'food', 3);
-	g.decreaseResource('worker', 1);
-	g.increaseResource('Farmer', 1);
-	if (g.resources.worker.amount <= 0) {
+	addToPool(myPools.farmer.key, 1);
+	if (g.pools.worker.amount <= 0) {
 		disableButton(addLumberjack);
 		disableButton(addFarmer);
 	}
@@ -97,9 +136,9 @@ var populateResourceList = function() {
 	listSection.appendChild(oldList);
     var list = document.createElement('ul');
 
-	for (var key in g.resources) {
-		var r = g.resources[key].name;
-		var a = g.resources[key].amount;
+	for (var key in g.pools) {
+		var r = g.pools[key].name;
+		var a = g.pools[key].amount;
 		var item = document.createElement('li');
 		item.appendChild(document.createTextNode(r + " : " + a));
 		list.appendChild(item);
@@ -110,21 +149,14 @@ var populateResourceList = function() {
 	list.setAttribute('id', 'resourceList');
 }
 
-var checkConditions = function(){
-	for (var key in g.resources) { // This.resources is undefined?
-			if (g.resources[key].conditions.add > 0) {
-				g.increaseResource(key, g.resources[key].conditions.add);
-			}
-			if (g.resources[key].conditions.minus > 0) {
-				g.decreaseResource(key, g.resources[key].conditions.minus);
-			}
-		}
-}
-
-var gameTick = function(){
-    tickCounter.innerHTML = gameTick;
+var gameTick = function(event, queue){
+	if (offset) {
+		tickCounter.innerHTML = tickTimer += 1;
+		offset = false;
+	} else {
+		offset = true;
+	}
     populateResourceList();
-    checkConditions();
 }
 
 document.addEventListener('tick', gameTick);
@@ -145,27 +177,28 @@ enableButton = function(elm) {
 // Testing
 
 // Adding pools
-g.addPool({
-  name: "Sir Steve",
-  key: "steve"
-});
+// g.addPool({
+//   name: "Sir Steve",
+//   key: "steve"
+// });
 
-g.addPool({
-  name: "MEGA-Wood",
-  key: "wood"
-})
+// g.addPool({
+//   name: "MEGA-Wood",
+//   key: "megawood",
+//   requirements: {steve: 1}
+// })
 
-// print on page
-function log(v) {
-  var d = document.querySelector('.log');
-  d.innerHTML += "<div>"+v+"</div>";
-}
+// // print on page
+// function log(v) {
+//   var d = document.querySelector('.log');
+//   d.innerHTML += "<div>"+v+"</div>";
+// }
 
-Object.keys(g.pools).forEach(e => log(g.pools[e].name +":"+ g.pools[e].amount));
-log("---");
+// Object.keys(g.pools).forEach(e => log(g.pools[e].name +":"+ g.pools[e].amount));
+// log("---");
 
-g.pools.wood.addToPool(5);
-g.pools.steve.addToPool(100);
+// g.pools.steve.addToPool(100);
+// g.pools.wood.addToPool(5);
 
-Object.keys(g.pools).forEach(e => log(g.pools[e].name +":"+ g.pools[e].amount));
-log("---");
+// Object.keys(g.pools).forEach(e => log(g.pools[e].name +":"+ g.pools[e].amount));
+// log("---");
--- engine.js
+++ engine.js
@@ -1,76 +1,140 @@
-console.log('Engine connected');
-
 (function(){
-// Incremental gaming engine
-
-// Create new instance of the game
-
-//TODO:
-//1) add/remove resource pools.
-// 2) raise/lower increments
-// 3) able to create "resources" that use other single, and multiple resources ( buildings use wood ect. )
-// 4) set custom "tick" time and process all the ups and downs required.
-// 5) manage multiple queues/pools ( maybe just a new instance or something? )
-// 6) able to retrieve all the data about the pools ( total, increment, ect )
-
-
-
-var poolPrototype = {
-  modifyPoolIncrement: function(n) {
-    this.increment += n;
-  },
-
-  addToPool: function(n) {
-  	if (n  > 0 && this.requirements) {
-  		for (var key in this.requirements) {
-  			if (this.requirements.hasOwnProperty(key)) {
-  				this.requirements.key
-  			}
-  		}
-  	}
-  	this.amount += n;
-  }
-}; // close prototype
 
 //Create Game Instance
 window.IncrementalEngine = function IncrementalEngine() {
 
-	this.gameInterval = 0;
-	this.gameTick = 0;
+	this.tickInterval = 0;
+	this.queueInterval = 0;
 	this.pools = {};
+	this.details ={};
 
 };
+
+// Create Pool Prototype
+var poolPrototype = {
+  modifyPoolIncrements: function(p, n) {
+    if (p === 'resetIncrements' && !Number.isInteger(n)) {
+  		this.increments = n;
+  	} else {
+  		this.increments[p] += n;
+  	}
+  },
+
+  modifyPoolDetails: function(key, val) {
+  	if (key === 'resetDetails' ) {
+  		this.increments = n;
+  	} else {
+  		this.increments[key] += val;
+  	}
+  },
+
+  modifyPoolRequirements: function(res, n) {
+  	this.requirements[res] += n;
+  },
+
+  modifyPoolMaximum: function(n, type) {
+  	if (type === 'set') {
+  		this.maximum = n
+  	} else {
+  		this.maximum += n
+  	}
+  },
+  modifyPoolMinimum: function(n, type){
+  	if (type === 'set') {
+  		this.minimum = n
+  	} else {
+  		this.minimum += n
+  	}
+  },
+
+  modifyPoolAmount: function(n, override) { 
+  	var requirements = this.checkRequirements(n);
+  	var pass = this.checkMinandMax(n);
+  	n = pass.success ? n : pass.toCapacity;
+
+  	if (requirements.success || override) {
+  		this.amount += n;
+  		return pass;
+  	} else {
+  		return requirements;
+  	}
+  },
+
+  checkRequirements: function(n){
+  	if (Object.keys(this.requirements).length) {
+  		var unmetReq = {};
+  		Object.keys(this.requirements).forEach(e => {
+  			if(this.queue.pools[e].amount < this.requirements[e] * n) {
+  				unmetReq[e] = (this.requirements[e] * n) - this.queue.pools[e].amount;
+  			}
+  		});
+  		if (!Object.keys(unmetReq).length) {
+  			Object.keys(this.requirements).forEach(e => this.queue.pools[e].modifyPoolAmount(-(this.requirements[e] * n)));
+  			return {success: true}
+  		} else {
+  			return {succeess: false, unmentRequirements: unmetReq};
+  		}
+  	} else {
+  		return {success: true};
+  	}
+  },
+
+  checkMinandMax: function(n){
+  	if (this.amount + n > this.maximum ) {
+  		return {success: false, pool: this, toCapacity: this.maximum - this.amount, surplus: this.amount + n - this.maximum};
+  	} else if (this.amount + n < this.minimum) {
+  		return {success: false, pool: this, toCapacity : this.minimum - this.amount, surplus: this.amount + n - this.minimum};
+  	} else {
+  		return {success: true }
+  	}
+  }
+}; // close prototype
+
+
 
 IncrementalEngine.prototype = {
 	//Begin a game session with Tick Interval in milliseconds
 	begin(n) {
-		this.gameInterval = window.setInterval(this.onTick, n);
+		this.tickInterval = n;
+		this.queueInterval = window.setInterval(this.onTick.bind(this), this.tickInterval);
 	},
 
 	// loops through tickConditions and calls required functions
 	onTick() {
-		Number.isInteger(this.gameTick) ? this.gameTick += 1 : this.gameTick = 0;
 
-		var event = new Event('tick', {tick: this.gameTick});
-		document.dispatchEvent(event);
-
-		for (var key in this.pools) { 
-			if (this.pools.hasOwnProperty(key) && this.pools[key].increment !== 0) {
-				this.pools[key].addToPool(this.pools[key].increment)
+		Object.keys(this.pools).forEach(el => {
+			if (this.pools[el].amount > 0 && this.pools[el].increments) {
+				Object.keys(this.pools[el].increments).forEach(e => {
+						if(this.pools[e].amount <= this.pools[e].maximum && this.pools[e].amount >= this.pools[e].minimum) this.pools[e].modifyPoolAmount(this.pools[el].increments[e] * this.pools[el].amount);
+				})
 			}
-		}
+		});
+
+		var event = new Event('tick', this);
+		document.dispatchEvent(event);
+	},
+
+	modifyQueueInterval(n){
+		window.clearInterval(this.queueInterval);
+		this.tickInterval = n;
+		this.queueInterval = window.setInterval(this.onTick.bind(this), this.tickInterval);
 	},
 
 	addPool(obj) {
-	  this.pools[obj.key? obj.key : obj.name] = Object.create(poolPrototype);
-	  Object.assign(this.pools[obj.key? obj.key : obj.name], {
+	  if (!this.pools[obj.key? obj.key : obj.name]) {
+	  	this.pools[obj.key? obj.key : obj.name] = Object.create(poolPrototype);
+	  	Object.assign(this.pools[obj.key? obj.key : obj.name], {
 	  	key: obj.key,
 	    name: obj.name,
-	    amount: 0,
+	    amount: obj.amount || obj.minimum || 0,
+	    minimum: obj.minimum || 0,
+	    maximum: obj.maximum || 100,
 	    requirements: obj.requirements || {},
-	    increment: obj.requirements || 0,
-	    details: obj.details || {}
+	    increments: obj.increments || {},
+	    details: obj.details || {},
+	    queue: this
   		});
+	  }
 	}
 
 
